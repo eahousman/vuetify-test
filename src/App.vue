6211e3a64b92416fa5bd4a3618e45078164414ed
Housman Eric <eric.housman@decisionlab.io>
2017-09-22 16:44:23 -0400

    Converted main template to pug

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,34 @@
-<template>
-  <v-app light>
-    <v-navigation-drawer temporary v-model="drawer" overflow absolute>
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar exact :to="{ name: 'Profile' }">
-            <v-list-tile-avatar>
-              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>John Doe</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
+<template lang="pug">
+  v-app(light)
+    v-navigation-drawer(temporary v-model="drawer" overflow absolute)
+      v-toolbar.transparent(flat)
+        v-list.pa-0
+          v-list-tile(avatar, exact, :to="{ name: 'Profile' }")
+            v-list-tile-avatar
+              img(src="https://randomuser.me/api/portraits/men/85.jpg")
+            v-list-tile-content
+              v-list-tile-title John Doe
 
-      <v-list class="pt-0">
-        <v-divider></v-divider>
-        <v-list-tile v-for="item in items" :key="item.title" exact :to="{ name: item.route }">
-          <v-list-tile-action>
-            <v-icon medium>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+      v-list.pt-0
+        v-divider
+        v-list-tile(exact, v-for="item in items", :key="item.title", :to="{ name: item.route }")
+          v-list-tile-action
+            v-icon(medium) {{ item.icon }}
+          v-list-tile-content
+            v-list-tile-title {{ item.title }}
 
-    <v-toolbar fixed>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-    </v-toolbar>
+    v-toolbar(fixed)
+      v-toolbar-side-icon(light @click.stop="drawer = !drawer")
+      v-toolbar-title(v-text="title")
 
-    <main>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <router-view></router-view>
-        </v-slide-y-transition>
-      </v-container>
-    </main>
+    main
+      v-container(fluid)
+        v-slide-y-transition(mode="out-in")
+          router-view
 
-    <v-footer fixed>
-      <v-spacer></v-spacer>
-      <div>Decision Lab&trade;</div>
-    </v-footer>
-  </v-app>
+    v-footer(fixed)
+      v-spacer
+      div Footnote
 </template>
 
 <script>
