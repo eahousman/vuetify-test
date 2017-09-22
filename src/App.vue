166187ca6a1a29c46049b822f4b12f46b513ca87
Housman Eric <eric.housman@decisionlab.io>
2017-09-22 12:53:37 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer persistent clipped v-model="drawer" enable-resize-watcher>
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>John Doe</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+
+    <main>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+
+    <v-footer fixed>
+      <v-spacer></v-spacer>
+      <div>Decision Lab&trade;</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        drawer: false,
+        items: [
+          { title: 'FAQ', icon: 'question_answer' }
+        ],
+        title: "Greg's Best Idea Ever"
+      };
+    }
+  };
+</script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .landing
+    h2 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'landing',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
